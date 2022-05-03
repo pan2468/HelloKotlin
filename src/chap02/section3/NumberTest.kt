e6b29b74830dd3f32e09f9e0d37999d998b8794d
임채성 <pan1129@naver.com>
2022-05-03 19:26:07 +0900

    자료형 검사하기 Commit

---
+++ src/chap02/section3/NumberTest.kt
@@ -0,0 +1,16 @@
+package chap02.section3
+
+fun main() {
+    var test: Number = 12.2
+    println("$test")
+
+    test = 12 //Int형으로 스마트 게스트
+    println("$test")
+
+    test = 120L // Long형으로 스마트 게스트
+    println("$test")
+
+    test += 12.0f
+    println("$test")
+
+}
