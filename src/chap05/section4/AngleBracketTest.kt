2655045ff3751ad313751cada1d55d56ccaf57ab
임채성 <pan1129@naver.com>
2022-05-05 19:03:05 +0900

    super,this 참조 Commit

---
+++ src/chap05/section4/AngleBracketTest.kt
@@ -0,0 +1,28 @@
+package chap05.section4
+
+open class A{
+    open fun f() = println("A Class f( )")
+    fun a( ) = println("A Class a( )")
+}
+
+interface B{
+    fun f() = println("B Interface f( )") //인터페이스 기본적으로 open 임
+    fun b() = println("B Interface b( )")
+}
+
+class C : A(), B { // 쉼표를 사용해 클래스와 인터페이스를 지정
+    // 컴파일되려면 f()가 오버라이딩되어야 함
+    override fun f() = println("C Class f( )")
+
+    fun test(){
+        f()
+        b()
+        super<A>.f()
+        super<B>.f()
+    }
+}
+
+fun main() {
+    val c = C()
+    c.test()
+}
