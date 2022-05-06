fa4a91463154d4724c2a51364d09402ec870d7b7
임채성 <pan1129@naver.com>
2022-05-06 16:03:31 +0900

    Set Commit

--- src/chap09/section2/ListLoop.kt
+++ src/chap09/section2/ListLoop.kt
@@ -2,7 +2,10 @@ package chap09.section2
 
 fun main() {
     val fruits = listOf("apple","banana","kiwi")
-    for(item in fruits){
-        println(item)
+//    for(item in fruits){
+//        println(item)
+//    }
+    for(index in fruits.indices){ // 인덱스 지정
+        println("fruits[$index] = ${fruits[index]}")
     }
 }
