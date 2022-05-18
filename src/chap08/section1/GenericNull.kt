2b812294201777df62a64c070f9031d2c63c65d1
임채성 <pan1129@naver.com>
2022-05-18 16:27:06 +0900

    형식 매개변수 null 가능 제네릭 Commit

--- src/chap08/section1/GenericNull.kt
+++ src/chap08/section1/GenericNull.kt
@@ -6,6 +6,12 @@ class GenericNull<T>{
     }
 }
 
+//class GenericNull<T: Any>{ // 자료형 Any가 지정되어 null 허용되지 않음
+//    fun EqualityFunc(arg1: T, arg2: T){
+//        println(arg1?.equals(arg2))
+//    }
+//}
+
 
 fun main() {
     val obj = GenericNull<String>() // non-null 선언됨
@@ -14,3 +20,5 @@ fun main() {
     val obj2 = GenericNull<Int?>() // null이 가능한 형식으로 선언됨
     obj2.EqualityFunc(null,10) // null 사용
 }
+
+
